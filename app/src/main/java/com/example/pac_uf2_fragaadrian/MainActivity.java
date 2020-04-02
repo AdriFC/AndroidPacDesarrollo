72b0e07dcfdb3ecb1f3badcd7aefa55a888d0a9f
Adri <adri86vigo@gmail.com>
2020-04-02 20:53:44 +0200

    trabajo en activity register e intent main-register

--- app/src/main/java/com/example/pac_uf2_fragaadrian/MainActivity.java
+++ app/src/main/java/com/example/pac_uf2_fragaadrian/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.pac_uf2_fragaadrian;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonRegister = findViewById(R.id.register_button);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                funcionRegister (v);
+            }
+        });
+    }
+    public void funcionRegister (View v){
+        Toast.makeText(this,getString(R.string.register_toast),Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent (MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
     }
 }
